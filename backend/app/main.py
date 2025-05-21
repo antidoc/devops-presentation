dc7e5fae8357f920b9d4fddd532d066b913753ca
Anton Dotsenko <dotsenko.anton@hotmail.com>
2025-05-21 23:45:05 +0300

    fix terraform

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,11 @@ def read_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return todos
 
 
-@app.post("/todos/", response_model=schemas.Todo, status_code=status.HTTP_201_CREATED)
+@app.post(
+    "/todos/",
+    response_model=schemas.Todo,
+    status_code=status.HTTP_201_CREATED
+)
 def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
     return crud.create_todo(db=db, todo=todo)
 
@@ -52,7 +56,11 @@ def read_todo(todo_id: int, db: Session = Depends(get_db)):
 
 
 @app.put("/todos/{todo_id}", response_model=schemas.Todo)
-def update_todo(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends(get_db)):
+def update_todo(
+        todo_id: int,
+        todo: schemas.TodoUpdate,
+        db: Session = Depends(get_db)
+):
     db_todo = crud.get_todo(db, todo_id=todo_id)
     if db_todo is None:
         raise HTTPException(status_code=404, detail="Todo not found")
